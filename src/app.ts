5704025c73c3fcef85ba84d25cad7985eceba896
din-islam-rajin <din.islam.rajin572@gmail.com>
2023-06-12 17:41:13 +0600

    [Done] project setup

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,14 @@
 import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
+
 const app: Application = express()
-const port = 3000
+
+//cors
+app.use(cors())
+
+//parser
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
 app.get('/', (req:Request, res:Response) => {
   res.send('University Management Server Auth System connected')
