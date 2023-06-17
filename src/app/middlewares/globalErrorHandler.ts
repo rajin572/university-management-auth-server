b1ded382d172e32e936fb65475a8ea5e656951c9
din-islam-rajin <din.islam.rajin572@gmail.com>
2023-06-17 19:11:22 +0600

    global errorHandler

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,12 +1,54 @@
 import { NextFunction, Request, Response } from 'express'
+import config from '../../config'
+import { IGenericErrorMessage } from '../../interfaces/error'
+import handleValidationError from '../../error/handleValidationError'
+import ApiError from '../../error/ApiError'
+import { Error } from 'mongoose'
 
 const globalErrorHandler = (
-  err,
+  err: Error.ValidationError,
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  res.status(400).json({ UN_EROOR: err })
+  let statusCode = 500
+  let message = 'Something Went Wrong'
+  let errorMessages: IGenericErrorMessage[] = []
+
+  if (err.name === 'ValidationError') {
+    const simplyfiedError = handleValidationError(err)
+    statusCode = simplyfiedError.statusCode
+    message = simplyfiedError.message
+    errorMessages = simplyfiedError.errorMessages
+  } else if (err instanceof ApiError) {
+    statusCode = err?.statusCode
+    message = err.message
+    errorMessages = err?.message
+      ? [
+          {
+            path: '',
+            message: err?.message,
+          },
+        ]
+      : []
+  } else if (err instanceof Error) {
+    message = err?.message
+    errorMessages = err?.message
+      ? [
+          {
+            path: '',
+            message: err?.message,
+          },
+        ]
+      : []
+  }
+
+  res.status(statusCode).json({
+    success: false,
+    message,
+    errorMessages,
+    stack: config.env !== 'production' ? err?.stack : undefined,
+  })
   next()
 }
 
