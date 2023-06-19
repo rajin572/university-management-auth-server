8ecbaceb856accc08537ada08378f20fc7bd81c7
din-islam-rajin <din.islam.rajin572@gmail.com>
2023-06-20 01:39:57 +0600

    zod validation handler

--- src/error/handleZodError.ts
+++ src/error/handleZodError.ts
@@ -1,12 +1,22 @@
+import { ZodError, ZodIssue } from 'zod';
 import { IGenericErrorResponse } from '../interfaces/common';
+import { IGenericErrorMessage } from '../interfaces/error';
 
-const handleZodError = (error): IGenericErrorResponse => {
+const handleZodError = (error: ZodError): IGenericErrorResponse => {
+  const errors: IGenericErrorMessage[] = error.issues.map(
+    (element: ZodIssue) => {
+      return {
+        path: element?.path.length - 1,
+        message: element?.message,
+      };
+    }
+  );
+
   const statusCode = 500;
-  console.log(error);
   return {
     statusCode,
     message: 'ZodError',
-    errorMessages: error,
+    errorMessages: errors,
   };
 };
 
