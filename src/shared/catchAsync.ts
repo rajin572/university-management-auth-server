12d13fb7801adaa49e34c1423bb4361c8e5023d8
din-islam-rajin <din.islam.rajin572@gmail.com>
2023-06-26 18:16:56 +0600

    Main to all branch

--- src/shared/catchAsync.ts
+++ src/shared/catchAsync.ts
@@ -1,17 +1,3 @@
-// import { NextFunction, Request, RequestHandler, Response } from 'express';
-
-// const catchAsync = (fun: RequestHandler) => {
-//   return async (req: Request, res: Response, next: NextFunction) => {
-//     try {
-//       fun(req, res, next);
-//     } catch (err) {
-//       next(err);
-//     }
-//   };
-// };
-
-// export default catchAsync;
-
 import { NextFunction, Request, RequestHandler, Response } from 'express';
 
 const catchAsync =
