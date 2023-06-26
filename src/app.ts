735012da2ec6f20d6e6ed2fe37251c0b21ab1e86
din-islam-rajin <din.islam.rajin572@gmail.com>
2023-06-26 18:48:15 +0600

    Main to all branch

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
-import express, { Application, Request, Response } from 'express';
+import express, { Application, NextFunction, Request, Response } from 'express';
 // import express, { Application, NextFunction, Request, Response } from 'express'
 import cors from 'cors';
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
 import routers from './app/routes/routes';
+import httpStatus from 'http-status';
 // import ApiError from './error/ApiError'
 const app: Application = express();
 
@@ -13,16 +14,30 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-//Application Router
-app.use('/api/v1', routers);
-
 // Default Route
 app.get('/', (req: Request, res: Response) => {
   res.send('University Management Application Auth system');
 });
 
-//global error handeler
+//Application Router
+app.use('/api/v1', routers);
 
+//global error handeler
 app.use(globalErrorHandler);
+
+//Not Found Error Handler
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(httpStatus.NOT_FOUND).json({
+    success: false,
+    message: 'Not Found',
+    errorMessages: [
+      {
+        path: req.originalUrl,
+        message: 'Api Not Found',
+      },
+    ],
+  });
+  next();
+});
 
 export default app;
